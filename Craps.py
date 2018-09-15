5925a79c2e2ad5aa5da1f22c3ec0fd91f971cf6f
yoyomaster9 <yoyomaster9@yahoo.com>
2018-09-14 19:19:50 -0700

    Botnagar 1.0

---
+++ Craps.py
@@ -0,0 +1,110 @@
+'''
+-------------
+Craps outline
+-------------
+
+1 - Create player database
+2 - Take bets & store into....classes..?
+    - Lock bet into class
+    - Ready check
+3 - Roll dice
+4 - Determine winners & loosers
+5 -
+
+-----
+Notes
+-----
+
+-- Include recurring bets
+
+RATIOS:
+
+
+
+--------
+Commands
+--------
+
+
+'''
+
+import random
+
+PUCK = 'OFF'
+
+players = {}
+
+class Bet:
+    def __init__(self, label, amt, odds):
+        pass
+
+class Player:
+    def __init__(self, bank, label):
+        self.bank = bank
+        self.label = label                                    # Pass  # Don't Pass
+        self.bets = {
+        'snakeeyes' :0,   # bet for 2         (one roll only)   31:1
+        'acedeuce' :0,    # bet for 3         (one roll only)   15:1
+        'four' :0,        # bet for 4         (standing)         2:1
+        'five' :0,        # bet for 5         (standing)         3:2
+        'six' :0,         # bet for 6         (standing)         6:5
+        'bigred' :0,      # bet for 7         (one roll only)    4:1
+        'eight' :0,       # bet for 8         (standing)         6:5
+        'nine' :0,        # bet for 9         (standing)         3:2
+        'ten' :0,         # bet for 10        (standing)         2:1
+        'yo' :0,          # bet for 11        (one roll only)   15:1
+        'boxcars' :0,     # bet for 12        (one roll only)   31:1
+        'hard4': 0,       # bet for 2 and 2   (standing)         7:1
+        'hard6': 0,       # bet for 3 and 3   (standing)         9:1
+        'hard8': 0,       # bet for 4 and 4   (standing)         9:1
+        'hard10': 0       # bet for 5 and 5   (standing)         7:1
+        }
+
+
+
+    def __str__(self):
+        s = '{name} has ${bank}'.format(name = self.label, bank = self.bank)
+        return s
+
+    def bet(self, type, amt):
+        # Don't make bets too large
+        if amt > self.bank:
+            return False
+
+        self.bets[type] = amt
+        self.bank = self.bank - amt
+
+        for bet in self.bets:
+            if self.bets[bet] < 0:
+                self.bets[bet] = 0
+
+    def payout(self, roll):
+        # roll is the number shown on dice
+        # roll is a tuple (d1, d2)
+        pay = 0
+        if
+
+    def leavegame(self):
+        pass
+
+
+
+
+
+p1 = Player(bank = 100, label = 'Thomas')
+^^^^
+p2 = Player(bank = 5, label = 'Harrison')
+
+str(4) - '4'
+
+p2.add1000()
+
+str(12) -> '12'
+
+p1.bank -> 100
+p2.bank -> 5
+
+players = []
+players.append(  player(bank = 100, label = 'Thomas') )
+
+Thomas.bank -> 100
---
+++ Krypto.py
@@ -0,0 +1,173 @@
+import itertools, time
+
+###############################################################
+#
+# Outline
+# =======
+# 1- Input numbers and target
+# 2- Permute numbers, set all permutations to list
+# 3- Permute operations, set all permutations to list
+# 4- For each permutations of number & operations, generate list
+#    of expressions under 14 paranthesis permutations
+# 5- Run eval() over each string generated in previous step
+# 6- Check if any of the eval() functions returned target card
+# 7- Output solution string
+#
+# NOTE:
+# Based on the Catalan number, C_4, there are 14 different ways to
+# have paranthesis on an expression with 5 terms
+#
+# SOURCE:
+# https://www.johndcook.com/blog/2013/10/03/parenthesize-expression-catalan/
+#
+###############################################################
+
+
+
+class Q:
+    def __init__(self, n, d = 1):
+        self.n = n
+        self.d = d
+        self.reduce()
+
+    def __add__(self, other):
+        n = (self.n * other.d) + (self.d * other.n)
+        d = self.d * other.d
+        return Q(n, d)
+
+    def __sub__(self, other):
+        other.n = -other.n
+        return self + other
+
+    def __mul__(self, other):
+        n = self.n * other.n
+        d = self.d * other.d
+        return Q(n, d)
+
+    def __truediv__(self, other):
+        other.n, other.d = other.d, other.n
+        return self * other
+
+    def reduce(self):
+        a = max(self.n, self.d)
+        b = min(self.n, self.d)
+        while b:
+            a, b = b, a % b
+        a = abs(a)
+        self.n = self.n // a
+        self.d = self.d // a
+
+    def __str__(self):
+        if self.d == 1:
+            return 'Q({})'.format(self.n)
+        else:
+            return 'Q({}, {})'.format(self.n, self.d)
+
+    def __eq__(self, other):
+        if self.n == other.n and self.d == other.d:
+            return True
+        else:
+            return False
+
+    def print(self):
+        if self.d == 1:
+            return '{}'.format(self.n)
+        else:
+            return '{}/{}'.format(self.n, self.d)
+
+
+
+# Generate list of possible expressions
+def GenerateExpressions(n, op):
+    Expressions = [
+    "{} {} ( {} {} ( {} {} ( {} {} {} )))".format(n[0], op[0], n[1], op[1], n[2], op[2], n[3], op[3], n[4]),
+    "{} {} ( {} {} (( {} {} {} ) {} {} ))".format(n[0], op[0], n[1], op[1], n[2], op[2], n[3], op[3], n[4]),
+    "{} {} (( {} {} {} ) {} ( {} {} {} ))".format(n[0], op[0], n[1], op[1], n[2], op[2], n[3], op[3], n[4]),
+    "{} {} (( {} {} ( {} {} {} )) {} {} )".format(n[0], op[0], n[1], op[1], n[2], op[2], n[3], op[3], n[4]),
+    "{} {} ((( {} {} {} ) {} {} ) {} {} )".format(n[0], op[0], n[1], op[1], n[2], op[2], n[3], op[3], n[4]),
+    "( {} {} {} ) {} ( {} {} ( {} {} {} ))".format(n[0], op[0], n[1], op[1], n[2], op[2], n[3], op[3], n[4]),
+    "( {} {} {} ) {} (( {} {} {} ) {} {} )".format(n[0], op[0], n[1], op[1], n[2], op[2], n[3], op[3], n[4]),
+    "( {} {} ( {} {} {} )) {} ( {} {} {} )".format(n[0], op[0], n[1], op[1], n[2], op[2], n[3], op[3], n[4]),
+    "( {} {} ( {} {} ( {} {} {} ))) {} {}".format(n[0], op[0], n[1], op[1], n[2], op[2], n[3], op[3], n[4]),
+    "( {} {} (( {} {} {} ) {} {} )) {} {}".format(n[0], op[0], n[1], op[1], n[2], op[2], n[3], op[3], n[4]),
+    "(( {} {} {} ) {} {} ) {} ( {} {} {} )".format(n[0], op[0], n[1], op[1], n[2], op[2], n[3], op[3], n[4]),
+    "(( {} {} {} ) {} ( {} {} {} )) {} {}".format(n[0], op[0], n[1], op[1], n[2], op[2], n[3], op[3], n[4]),
+    "(( {} {} ( {} {} {} )) {} {} ) {} {}".format(n[0], op[0], n[1], op[1], n[2], op[2], n[3], op[3], n[4]),
+    "((( {} {} {} ) {} {} ) {} {} ) {} {}".format(n[0], op[0], n[1], op[1], n[2], op[2], n[3], op[3], n[4])
+    ]
+    return Expressions
+
+
+
+# Evaluate each expression
+def Solver(NUMBERS, TARGET, StopAfterSolution = True):
+    trial = 0
+    Solutions = [] # list containing all solutions
+
+    # Permute operations
+    OPERATIONS = list(itertools.product(['+', '-', '*', '/'], repeat = 4))
+    # OPERATSIONS is the list of all permutations of operations
+
+    for n in NUMBERS:
+        for op in OPERATIONS:
+                Expressions = GenerateExpressions(n, op)
+                for exp in Expressions:
+                    trial += 1
+                    try:
+                        x = eval(exp)
+                    except ZeroDivisionError:
+                        x = -1
+                    if x == TARGET:
+                        Solutions.append({'trial': trial, 'exp': exp})
+                        if StopAfterSolution:
+                            return Solutions
+    return Solutions
+
+def StrToQ(s):
+    s = s.split('/')
+    if len(s) == 1:
+        return Q(int(s[0]))
+    else:
+        return Q( int(s[0]) , int(s[1]))
+
+def InterpretSolution(s):
+    s = s.replace('/', '÷')
+    for i in range(s.count('Q')): # Fix to # of Q's in string
+        start = s.index('Q')
+        end = s.index(')', start)
+        sub = s[start:end+1]
+        x = eval(sub).print()
+        s = s.replace(sub, x, 1)
+    return s
+
+
+
+
+s = input('What are the numbers? Write fractions as 2/3 with commas between.\n')
+
+s = s.replace(' ', '')
+NUMBERS = []
+for x in s.split(','):
+    NUMBERS.append(StrToQ(x))
+
+# Permute numbers
+NUMBERS = list(itertools.permutations(NUMBERS))
+# NUMBERS is the list of all permutations of NUMBERS
+
+TARGET = input('What is the target number?\n')
+TARGET = StrToQ(TARGET)
+
+
+StopAfterSolution = True
+print('\nFinding solution...')
+StartTime = time.time()
+try:
+    Solutions = Solver(NUMBERS, TARGET, StopAfterSolution)
+    print('Solution found!\n')
+    Solution = '{} = {}'.format(Solutions[0]['exp'], TARGET)
+    print('Trial {}: {}'.format(Solutions[0]['trial'], InterpretSolution(Solution)))
+except:
+    print('No solution found.')
+
+StopTime = time.time()
+print('Time elapsed: {0:.2f} seconds'.format(StopTime - StartTime))
