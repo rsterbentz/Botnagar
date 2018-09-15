86911eda7ed952c8a9bcb6469054f1aeb39bb6b2
yoyomaster9 <yoyomaster9@yahoo.com>
2018-09-15 00:12:03 -0700

    Moved all quotes, responses, and nicknames to separate python file

--- Botnagar.py
+++ Botnagar.py
@@ -1,9 +1,13 @@
 # Must be run in python 3.4 - 3.6
 
 import discord, random
-import Tokens # Tokens.py contains discord tokens for beta and release bhat - not in git
-# import Krypto
-# ^ To be implemented
+
+# Tokens.py contains private discord tokens for beta and release bhat - not in git
+import Tokens
+
+# Data.py contains quotes, 8ball responses, and nicknames
+from Data import *
+
 
 # Beta switches to beta Botnagar
 BETA = False
@@ -14,107 +18,6 @@ else:
     TOKEN = Tokens.RELEASE
 
 client = discord.Client()
-
-users = {
-'Porsche':'Dom',
-'Jumping Dog':'Etin Window',
-'Renskay':'Etin Spyder',
-'Tovar':'Rodger',
-'yolomaster':'Garry',
-'Desk':':b: :regional_indicator_r: :regional_indicator_e: :regional_indicator_a: :regional_indicator_d:',
-'tuxy':':b: oston'
-}
-
-quotes = [
-'*You know, {AUTHOR}, there is so much to look around!*',
-'*Yes, {AUTHOR}, this is really the real substance of life!*',
-'*Unfortunately, {AUTHOR}, there is no fanciful term we can use for this!*',
-'*{AUTHOR}, I want you to think about this that you are saying... it.*',
-'*If you think about it, {AUTHOR}, so many people came and went away.*',
-'*You know, {AUTHOR}, that is something that you will have your stamp on it!*',
-'*You know, {AUTHOR}, not all of them do, but they will.*',
-'*Watch my remarks, {AUTHOR}.*',
-'*{AUTHOR}, I saw a twinkle in my eye!*',
-'*{AUTHOR}, please give me three or five lines of gist on it.*',
-'*{AUTHOR}, today I want you to set the bar rolling!*',
-'*Unfortunately, {AUTHOR}, we sometimes take it for granted these things.*',
-'*You see, {AUTHOR}, this is something you either know it or don\'t!*',
-'*What are the Jews doing that we are not? You know this answer, {AUTHOR}?*',
-'*Remember, {AUTHOR}, 60\% of the Jews own banks.*',
-'*Remember, {AUTHOR}, 60\% of banks are owned by the Jews.*',
-'*India doesn\'t have any computers. {AUTHOR}, did you know this?*',
-'*{AUTHOR}, you need to reach inside and find the nugget!*',
-'*You know, {AUTHOR}, a bird in the hand is WORTH IT!*',
-'*They say, {AUTHOR}, what comes around GOES!*',
-'*I\'m tired, {AUTHOR}, I think I might go hit my sack.*',
-'*Yes, {AUTHOR}, this was the straw that broke the camel.*',
-'*I\'m feeling a bit OVER the weather today, {AUTHOR}!*',
-'*You are really beating around my bush, {AUTHOR}.*',
-'*You know, {AUTHOR}, they say... err...*',
-'*We will burn that bridge when we get there, {AUTHOR}.*',
-'*You know, {AUTHOR}, a penny saved is a penny!*',
-'*I believe, {AUTHOR}, that this is a blessing of the disguise!*',
-'*It is as they say, {AUTHOR}, better late... yes!*',
-'*Like they say, {AUTHOR}, it is water on the bridge.*',
-'*Did you know that {randcomposite} is prime?*',
-'*Sometimes we bark up the RIGHT tree, {AUTHOR}!*',
-'*You know, {AUTHOR}, birds of a feather fly north!*',
-'*Remember {AUTHOR}, actions speak loud!*',
-'*I will have to take a checked rain on that one, {AUTHOR}!*'
-'*Always take me with a grain of pepper, {AUTHOR}.*',
-'*{AUTHOR}, we see eye to it!*',
-'*I hate it when someone gives me the cold shudder, {AUTHOR}.*',
-'*{AUTHOR}, have you ever tasted your own medicine?*',
-'*{AUTHOR}, you are off of your ROCKS...err*',
-'*You know, {AUTHOR}, you should always do unto others as you would have done unto yourself... err...*',
-'*As the blacks say, the ball is in your fort {AUTHOR}!*',
-'*Is there a method to your mad mess {AUTHOR}?*',
-'*You know, {AUTHOR}, you can lead a horse to water, but you can\'t drink him!*',
-'*{AUTHOR} You really opened my worm in the can!*',
-'*{AUTHOR}, it is like the pot calling the kettle a nigger... err...*',
-'*Don\'t count your chickens before they are chickens, {AUTHOR}.*',
-'*It is like they say in India, you need to live and let fly, {AUTHOR}.*',
-'*As John Lennon once said, {AUTHOR}, you may call me a dreamer, but I\'m not.*',
-'*{AUTHOR}, please don\'t cry in the milk.*',
-'*Don\'t forget, {AUTHOR}, every cloud has a silver nugget!*',
-'*{AUTHOR}, it isn\'t over until my lady fat sings.*',
-'*Please stop, {AUTHOR}. I have bigger fish to eat.*',
-'*{AUTHOR}, my balls are in your court now.*',
-'*I\'m a little hungry, {AUTHOR}. I could really go for some chips off the old block!*',
-'*You know, {AUTHOR}, a picture paints a thousand pictures!*',
-'*I think you are stuck between a rock and my hard place, {AUTHOR}.*',
-'*Look outside, {AUTHOR}! It\'s raining cats and horses!*',
-'*{AUTHOR}, an apple a day keeps my doctors at bay!*',
-'*{AUTHOR}, please don\'t let the cat out of it\'s bag...*',
-'*{AUTHOR}, I prefer to do things at the drop of my bhat! Get it!?*',
-'*Maybe we need to address this elephant\'s room, {AUTHOR}.*',
-'*Have you ever taken your caution and thrown it at the wind, {AUTHOR}?*',
-'*You know, {AUTHOR}, it takes two to play the bongos!*',
-'*You know, they say, {AUTHOR}, don\'t cover a book after you judge it!*',
-'*This is just the calm after the storm, {AUTHOR}.*'
-'*You know, {AUTHOR}, you really threw me under the boat there.*'
-]
-
-ball = [
-'*I believe it is yes!*',
-'*Err.. I do not believe so*',
-'*I do no possess the knowledge to be the answerer*',
-'*Ask again when you have TENURE... Did you know that I have TENURE??*',
-'*{AUTHOR}, you are of the knowing of it!*',
-'*Ask me later, {AUTHOR}...*',
-'*You know... There ARE people who think yes!*',
-'*{AUTHOR}, you may be doing it!*'
-]
-
-PrimeScore = {
-'Dom':0,
-'Etin Window':0,
-'Etin Spyder':0,
-'Rodger':0,
-'Garry':0,
-':b: :regional_indicator_r: :regional_indicator_e: :regional_indicator_a: :regional_indicator_d:':0,
-':b: oston':0
-}
 
 def IsPrime(n):
     m=int(n**0.5)
